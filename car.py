76de3552081aac246b307e7bafeea998ed69050a
unknown <sushmithabanger@gmail.com>
2019-07-24 14:05:39 +0530

    aaaaa

---
+++ car.py
@@ -0,0 +1,34 @@
+class car:
+
+
+    def __init__(self,regno,no_gears):
+        self.regno=regno
+        self.no_gears=no_gears
+        self.is_started=False
+
+    
+    def start(self):
+        if self.is_started:
+            print(f"car is already started.....")
+        else:
+            print(f"car with regno :{self.regno} started.....")
+            self.is_started=True
+            
+    def stop(self):
+        if self.is_started:
+            print(f"car with regno {self.regno} stopped...")
+            self.is_started=False
+        else:
+            print(f"car with regno {self.regno} already stopped...")
+
+
+    def change_gear(self):
+        if self.is_started:
+            if self.c_gear<self.no_gears:
+                self.c_gear+=1
+                print(f"car with regno {self.regno} changed gear...{self.c_gear}")
+            else:
+                print(f"car with regno : {self.regno} already ")
+            self.is_started=False
+        else:
+            print("you cant change the gear")
---
+++ manager.py
@@ -0,0 +1,12 @@
+from car import car
+if __name__=="__main__":
+    bmw=car("ka013060",4)
+    bmw.start()
+    bmw.start()
+    bmw.change_gear()
+    bmw.change_gear()
+    bmw.change_gear()
+    bmw.change_gear()
+    bmw.change_gear()
+    bmw.stop()
+    bmw.stop()
